455777f2fb3442f5a9fe6f62d75a860b5c7495e8
zyrask <kingnoobery@gmail.com>
2025-07-20 01:44:24 -0500

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,18 @@
-// This file is required for Vercel deployment
-// It exports the Express app to be used as a serverless function
+import express, { type Request, Response, NextFunction } from "express";
+import { registerRoutes } from "../server/routes.js";
 
-import app from '../server/index.js';
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Set up routes
+await registerRoutes(app);
+
+// Error handling middleware
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  const status = err.status || err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(status).json({ message });
+});
 
 export default app;
